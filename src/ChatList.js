fb110689bfca8f279e01ea671fc582c9368b0c2b
Kostyantyn <kutuz00@gmail.com>
2021-09-20 09:28:47 -0700

    Starting Lesson 6

--- src/ChatList.js
+++ src/ChatList.js
@@ -1,17 +1,36 @@
-import React from "react";
-import { List, ListItem } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, List, ListItem } from "@material-ui/core";
 import './App.css';
 import { Link } from "react-router-dom";
 
-export const ChatList = ({ chats }) => {
+export const ChatList = ({ chats, addChat, deleteChat }) => {
+    const [value, setValue] = useState('');
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefaults();
+        if (value) {
+            addChat(value);
+        }
+
+    }
     return (
-        <div className="list"> <List>
-            {chats.map((chat) =>
 
-                <Link key={chat.id} to={`/chats/${chat.id}`}><ListItem key={chat.id}>{chat.chatName}  </ListItem></Link>
+        <div>
+            <div className="list App-link"> <List>
+                {chats.map((chat) => {
+                    ChatItem
+                    <><form className='form'>
+                        <input className='input' type='text' value={value} />
+                        <Button variant='contained'>Add chat</Button>
+                    </form>
+                        <ListItem className='button' key={chat.id}>{chat.chatName}  <Link key={chat.id} to={`/chats/${chat.id}`}>
+                        </ListItem>
+                }
 
-
-            )}
-        </List ></div>
+                </>
+            </List >
+        </div></div >
     )
 }
